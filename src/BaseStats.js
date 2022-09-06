a65830b41d153150ab467594163778cb04f16b3b
kllivecka <aklivecka@gmail.com>
2022-09-06 19:31:22 +0000

    Update BaseStats.js

--- src/BaseStats.js
+++ src/BaseStats.js
@@ -1,3 +1,12 @@
+
+
 const BaseStats = (props) => {
     
+    return (
+        <div>
+            <h1></h1>
+        <button onClick = {clicker}> special stats</button>
+        </div>
+    )
 }
+ export default BaseStats
